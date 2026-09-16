5f439545b4a386cdb8065b2445ce751e7e1b5394
nelcebu <n.escobar@unimayor.edu.co>
2026-09-16 00:08:26 -0500

    Generador de numeros enteros aleatorios
    Este metodo se va a usar en todos los ejercicios que piden utilizar numeros enteros aleatorios, que en este caso seran 3, 4, 5, 6, 8, 9.

---
+++ src/arreglos/GeneradorNumeros.java
@@ -0,0 +1,22 @@
+package arreglos;
+
+import java.util.Random;
+
+public class GeneradorNumeros {
+
+    private static final Random generadorAleatorio = new Random();
+
+    public static int[] generarNumerosAleatorios(int cantidad, int minimo, int maximo) {
+        int[] numeros = new int[cantidad];
+
+        for (int posicion = 0; posicion < cantidad; posicion++) {
+            numeros[posicion] = generarNumeroEnRango(minimo, maximo);
+        }
+
+        return numeros;
+    }
+
+    private static int generarNumeroEnRango(int minimo, int maximo) {
+        return generadorAleatorio.nextInt(maximo - minimo + 1) + minimo;
+    }
+}
